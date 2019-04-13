ab82eedcd4d5f2db00ba5ac851a12d13ed379425
Caleb Einarr <earthctzn1@gmail.com>
2019-04-13 02:48:19 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,6 +1,5 @@
 class Hash
   def keys_of(*arguments)
-    # Create new array of keys when arguments match hash values. Replace false values with nil and then remove with compact.
     self.map { |key, value| arguments.include?(value) ? key : nil }.compact
   end
 end 
